6b564d38e2bb5350ca39c72df9d604b1ec0e39ba
Jessica Schaefer <jschaefer@3pc.de>
2020-05-21 15:25:16 +0200

    add model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+# rails generate model Cocktail name:string
+# rails generate model Ingredient name:string
+# rails generate model Dose description:text ingredient:references cocktail:references
+
+# cocktail
+# validates :name, presence: true , uniqueness: true
+# has_many :dose, dependent: :destroy
+
+# ingredient
+# validates :name, presence: true , uniqueness: true
+# has_many :dose, dependent: :destroy
+
+# dose
+# validates :description, presence: true , uniqueness: true
+# belongs_to :cocktail
+# belongs_to :ingredient
+# validates :cocktail, presence: true , uniqueness: true
+# validates :ingredient, presence: true , uniqueness: true
